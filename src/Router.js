8d9f8d2b90eda5c8c1ff2a8130f853e347ce86f7
Merilee Wheelock <eelirem@gmail.com>
2018-07-11 16:33:23 -0700

    employee list

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 // this router will be the top level component inside the app
 // react-native-router-flux must have one parent/root Scene
@@ -8,7 +9,8 @@ const RouterComponent = () => {
 	return (
 		<Router>
 			<Scene key="root">
-				<Scene key="login" component={LoginForm} title="Please Login" />
+				<Scene key="login" component={LoginForm} title="Please Login" initial />
+				<Scene key="employeeList" component={EmployeeList} title="Employees" />
 			</Scene>
 		</Router>
 	);
