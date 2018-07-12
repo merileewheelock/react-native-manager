de0471b6f299d5af32ca1e81e6016b0b178f1ec1
Merilee Wheelock <eelirem@gmail.com>
2018-07-12 16:06:09 -0700

    saving users to firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,5 +1,6 @@
 // this is one action creator to handle any update to the form
 // if update name, will send a prop of "name" and a value of the new name
+import firebase from 'firebase';
 import {
 	EMPLOYEE_UPDATE
 } from './types';
@@ -8,5 +9,14 @@ export const employeeUpdate = ({ prop, value }) => {
 	return {
 		type: EMPLOYEE_UPDATE,
 		payload: { prop, value }
-	}
+	};
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+	// console.log(name, phone, shift);
+	const { currentUser } = firebase.auth();
+
+	// the below is a path to our JSON data store
+	firebase.database().ref(`/users/${currentUser.uid}/employees`)
+		.push({ name, phone, shift });
 };
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+	onButtonPress() {
+		const { name, phone, shift } = this.props;
+
+		this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+		// use shift given in form; if not set, use Monday
+	}
+
 	render() {
 		return (
 			<Card>
@@ -23,7 +30,7 @@ class EmployeeCreate extends Component {
 						label="Phone"
 						placeholder="555-555-5555"
 						value={this.props.phone}
-						ohChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
+						onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
 					/>
 				</CardSection>
 
@@ -44,7 +51,7 @@ class EmployeeCreate extends Component {
 				</CardSection>
 
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Create
 					</Button>
 				</CardSection>
@@ -66,4 +73,6 @@ const mapStateToProps = (state) => {
 	return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { 
+	employeeUpdate, employeeCreate 
+})(EmployeeCreate);
