cb5b767d67d314133cf535779536bea732fd05a9
Merilee Wheelock <eelirem@gmail.com>
2018-07-12 17:24:42 -0700

    employee reducer

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,8 +1,11 @@
 // this is one action creator to handle any update to the form
 // if update name, will send a prop of "name" and a value of the new name
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
-	EMPLOYEE_UPDATE
+	EMPLOYEE_UPDATE,
+	EMPLOYEE_CREATE,
+	EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -16,7 +19,27 @@ export const employeeCreate = ({ name, phone, shift }) => {
 	// console.log(name, phone, shift);
 	const { currentUser } = firebase.auth();
 
-	// the below is a path to our JSON data store
-	firebase.database().ref(`/users/${currentUser.uid}/employees`)
-		.push({ name, phone, shift });
+	// this is a requirement of redux-thunk (middleware for async actions)
+	return (dispatch) => {
+		// the below is a path to our JSON data store
+		firebase.database().ref(`/users/${currentUser.uid}/employees`)
+			.push({ name, phone, shift })
+			.then(() => {
+				dispatch({ type: EMPLOYEE_CREATE });
+				Actions.pop() // returns to previous scene
+			});
+	};
+};
+
+export const employeesFetch = () => {
+	const { currentUser } = firebase.auth();
+
+	return (dispatch) => {
+		firebase.database().ref(`/users/${currentUser.uid}/employees`)
+			// anytime data comes from this ref, call fat arrow function with object (snapshot)
+			// to describe the data in there
+			.on('value', snapshot => {
+				dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() })
+			});
+	};
 };
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,5 +1,6 @@
 import {
-	EMPLOYEE_UPDATE
+	EMPLOYEE_UPDATE,
+	EMPLOYEE_CREATE
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -14,6 +15,8 @@ export default (state = INITIAL_STATE, action) => {
 			// action.payload will === { prop: 'name', value: 'jane' }
 			return { ...state, [action.payload.prop]: action.payload.value };
 			// square braces are NOT AN ARRAY, this is key interpolation
+		case EMPLOYEE_CREATE:
+			return INITIAL_STATE; // reset state to clear out form after submission
 		default: 
 			return state;
 	}
